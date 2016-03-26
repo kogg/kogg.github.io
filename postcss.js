61caa79c8b07b72fd7bde99ef6c49f8414c3e83b
Saiichi Hashimoto <saiichihashimoto@gmail.com>
2016-03-26 04:23:15 -0700

    postcss

---
+++ postcss.js
@@ -0,0 +1,11 @@
+module.exports = {
+	'use': [
+		'postcss-nested',
+		'autoprefixer',
+		'postcss-mq-keyframes',
+		//'cssnano',
+		'postcss-reporter'
+	],
+	'cssnano':          { autoprefixer: false },
+	'postcss-reporter': { clearMessages: true }
+};
